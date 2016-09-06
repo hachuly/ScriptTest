ccd36f109dc75638eb4bbfaada5ac96efcebe272
logbeam <desu_taratyann@yahoo.co.jp>
2016-09-06 20:10:40 +0900

    ScriptTest C#基礎
    課題と＋α

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    private int[] array = {1,3,5,7,13};
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("arrayの要素\n");
+        foreach(int i in array){
+            Debug.Log(i);
+        }Debug.Log("\n");
+        Debug.Log("逆順表示\n");
+        for(int i = array.Length-1; i >= 0; i--){
+            Debug.Log(array[i]);
+        }
+	}
+
+}
---
+++ Assets/Test_another.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_another : MonoBehaviour {
+
+    private int mp = 53;
+
+    void Magic(){
+        if(0 <= (mp = mp - 5) ){
+            Debug.Log("魔法攻撃をした。残りMPは"+ mp +"。");
+         }else{
+            Debug.Log("MPが足りないため魔法が使えない。");
+         }
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < 11; i++){
+            Magic();
+        }
+	}
+
+}
